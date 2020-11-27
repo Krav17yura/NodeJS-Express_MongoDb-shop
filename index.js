485bb45c09d5afb91d792f37504d8035d23ecdde
Yura Krav <krav17yura@mgail.com>
2020-11-27 11:05:48 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const path = require('path')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const csurfe = require('csurf')
+const flash = require('connect-flash')
+const Hendlebars = require('handlebars')
+const session = require('express-session')
+const MongoStore = require('connect-mongodb-session')(session)
+const {allowInsecurePrototypeAccess} = require('@handlebars/allow-prototype-access')
+const homeRoutes = require('./routes/home')
+const cardRoutes = require('./routes/card')
+const addRoutes = require('./routes/add')
+const ordersRoutes = require('./routes/orders')
+const coursesRoutes = require('./routes/courses')
+const authRouter = require('./routes/auth')
+const varMiddleware = require('./middleware/variables')
+const userMiddleware = require('./middleware/user')
+const keys = require('./keys')
+
+const app = express()
+
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    handlebars: allowInsecurePrototypeAccess(Hendlebars),
+    extname: 'hbs'
+})
+
+const store = new MongoStore({
+    collection: 'sessions',
+    uri: keys.MONGODB_URL
+
+})
+
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+
+
+app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended: true}))
+app.use(session({
+    secret: keys.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store
+}))
+app.use(csurfe())
+app.use(flash())
+app.use(varMiddleware)
+app.use(userMiddleware)
+
+app.use('/', homeRoutes)
+app.use('/add', addRoutes)
+app.use('/courses', coursesRoutes)
+app.use('/card', cardRoutes)
+app.use('/orders', ordersRoutes)
+app.use('/auth', authRouter)
+
+
+const PORT = process.env.PORT || 3000
+
+async function start() {
+    try {
+
+        await mongoose.connect(keys.MONGODB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`)
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+start()
---
+++ keys/index.js
@@ -0,0 +1,7 @@
+module.exports = {
+    MONGODB_URL: 'mongodb+srv://krav:3xdmDcHLrhNznVsl@cluster0.nbhmm.mongodb.net/shop',
+    SESSION_SECRET: 'some secret value',
+    SENDGRID_API_KEY: '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',
+    EMAIL_FROM: 'nodeJS-courses-krav17yura@dada.com',
+    BASE_URL: 'http://localhost:3000/'
+}
